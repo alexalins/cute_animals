8b8f30ccd720d7a3d827848050000476b5105751
Alius <alexa.glinsoo@gmail.com>
2020-07-22 21:34:49 -0300

    iniciando DogPage

--- lib/page/DogPage.dart
+++ lib/page/DogPage.dart
@@ -6,10 +6,24 @@ class DogPage extends StatefulWidget {
 }
 
 class _DogPageState extends State<DogPage> {
+  newDog() {
+    print("NEW DOG!");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+          child: Center(
+        child: new Image(image: new NetworkImage("https://images.dog.ceo/breeds/pyrenees/n02111500_5618.jpg")),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          newDog();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blueAccent,
+      ),
     );
   }
 }
